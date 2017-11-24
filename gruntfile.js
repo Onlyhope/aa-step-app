26d8faa96b29f3b1d97dd4b034828598388ba426
Aaron Lee <leeaaron326@gmail.com>
2017-11-24 16:59:10 -0500

    adding gitignore and sass dir

--- gruntfile.js
+++ gruntfile.js
@@ -8,19 +8,42 @@ module.exports = function(grunt) {
           livereload: true, // needed to run LiveReload
         }
       }
-    }
+    },
+
+    nodemon: {
+      dev: {
+        script: 'app.js',
+        options: {
+          ignore: ['node_modules/**', 'public', 'src/browser']
+        }
+      }
+    },
 
     sass: {
       dist: {
         files: {
-          'public/stylesheets/aa-style.css' : 'sass/style.scss'
+          'public/stylesheets/style.css' : 'sass/style.scss'
+        }
+      }
+    },
+
+        concurrent: {
+      dev: {
+        tasks: ['nodemon'],
+        options: {
+          logConcurrentOutput: true
         }
       }
     }
+
   });
 
+
+  grunt.loadNpmTasks('grunt-nodemon');
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['sass', 'watch']);
+  grunt.loadNpmTasks('grunt-concurrent');
+
+  grunt.registerTask('default', ['sass', 'concurrent:dev']);
 };
 
