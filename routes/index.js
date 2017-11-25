3a3823f3085e5a3889b673708002b52aadf355d6
Onlyhope <leeaaron326@gmail.com>
2017-11-25 13:10:04 -0500

    Upload is now working

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
+var path = require('path');
+var formidable = require('formidable');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   	res.render('pages/index', { 
   							title: "Express",
   							author: "Aaron Lee"
   						});
+});
+
+router.post('/file-upload', function(req, res, next) {
+	var form = new formidable.IncomingForm();
+	var upload_dir = path.join(__dirname, "../uploads/");
+
+	form.parse(req, function(err, fields, files) {
+		var oldpath = files.filetoupload.path;
+		var newpath = upload_dir + files.filetoupload.name;
+		fs.rename(oldpath, newpath, function(err) {
+			if (err) throw err;
+			res.write('File uploaded and moved!');
+			res.end();
+		});
+	});
 });
 
 module.exports = router;
