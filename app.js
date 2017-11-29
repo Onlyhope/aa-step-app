2e05cd19dbbb5affcb006c499d94e5fd90462d45
Onlyhope <leeaaron326@gmail.com>
2017-11-29 11:39:35 -0500

    playing around with csv parsing

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var csv = require('csv');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -42,5 +43,36 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('pages/error');
 });
+
+/* CSV Parsing */
+require('should');
+
+var output = [];
+// Create the parser
+var parser = csv.parse({delimiter: ':'});
+// Use the writable stream api
+parser.on('readable', function() {
+	while (record = parser.read()) {
+		console.log(record);
+		output.push(record);
+	}
+});
+// Catch any error
+parser.on('error', function(err) {
+	console.log(err.message);
+});
+// When we are done, test that he parsed output matched what expected
+parser.on('finish', function(){
+  	output.should.eql([
+    	[ 'root','x','0','0','root','/root','/bin/bash' ],
+    	[ 'someone','x','1022','1022','a funny cat','/home/someone','/bin/bash' ]
+  	]);
+});
+
+// Now that setup is done, write data to the stream
+parser.write("root:x:0:0:root:/root:/bin/bash\n");
+parser.write("someone:x:1022:1022:a funny cat:/home/someone:/bin/bash\n");
+// Close the readable stream
+parser.end();
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ var router = express.Router();
 var fs = require('fs');
 var path = require('path');
 var formidable = require('formidable');
+var csv = require('csv');
+
+var upload_dir = path.join(__dirname, "../uploads/");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,13 +14,29 @@ router.get('/', function(req, res, next) {
 		{ username: "Aaron Lee", password: "123", age: 5 },
 		{ username: "Benjamin Lee", password: "456", age: 10 },
 		{ username: "Chris Lee", password: "789", age: 15 }
-	]
+	];
+
+	var list_of_files = [];
+	fs.readdirSync(upload_dir).forEach(function(file) {
+		list_of_files.push(file);
+	});
 
   	res.render('pages/index', { 
   							title: "Express",
   							author: "Aaron Lee",
-  							userList: list_of_users
+  							userList: list_of_users,
+  							fileList: list_of_files
   						});
+});
+
+router.get('/file-display/:file_name', function(req, res, next) {
+	var file_path = upload_dir + req.params.file_name;
+	console.log(file_path);
+
+	fs.readFile(file_path, (err, data) => {
+		if (err) throw err;
+		res.send("<h2>" + data + "</h2>");
+	});
 });
 
 router.get('/profile/:name', function(req, res, next) {
@@ -26,19 +45,15 @@ router.get('/profile/:name', function(req, res, next) {
 
 router.post('/file-upload', function(req, res, next) {
 	var form = new formidable.IncomingForm();
-	var upload_dir = path.join(__dirname, "../uploads/");
 
 	form.parse(req, function(err, fields, files) {
 		var oldpath = files.filetoupload.path;
 		var newpath = upload_dir + files.filetoupload.name;
 		fs.rename(oldpath, newpath, function(err) {
 			if (err) throw err;
-			res.write('File uploaded and moved!');
-			res.end();
+			res.redirect('/');
 		});
 	});
 });
-
-
 
 module.exports = router;
